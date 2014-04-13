abdb745ab6ed01c83f46a1f6f22a70db1825830e
yihzhang <yihzhang@indiana.edu>
2014-04-13 16:18:14 -0400

    added idea crossref, changed search

--- crossRef.js
+++ crossRef.js
@@ -1,266 +1,256 @@
 (function (window, $) {
-    /*
-      $occurrences = inpho("/thinker/$id/occurrences.json?sep_filter=True");
-      $related = inpho("/thinker/$id/related.json?sep_filter=True");
-      $hyponyms = inpho("/thinker/$id/hyponyms.json?sep_filter=True");
-      $influenced = inpho("/thinker/$id/influenced.json?sep_filter=True");
-    */
 
-    //global variables
-    var host = "https://inpho.cogs.indiana.edu";
+   //global variables
+   var host = "https://inpho.cogs.indiana.edu";
 
-    var crossRef = angular.module("crossRef", []);
-    crossRef.controller("crossRefCtrl", function ($scope, $http, $q, $timeout) {
+   var crossRef = angular.module("crossRef", []);
+   crossRef.controller("crossRefCtrl", function ($scope, $http, $q, $timeout) {
 
-	$scope.currentThinkerID = "3345";
-	$scope.newThinkerID = "";
+      $scope.sep_dir = "kant";
+      $scope.entity = {};
 
-	$scope.currentThinker = {}; //initialize to an empty array.
+      $scope.entities = [];
+      $scope.labels = [];
+      $timeout(function () { getEntities($scope, $http, $q) }, 50);
 
-	$scope.currentIdeaID = ""; 
-	$scope.newIdeaID = "";
+      $scope.thinkerColumns = [];
+      $scope.ideaColumns = [];
 
-	$scope.currentIdea = {}; 
+      $scope.getCurrentEntity = function () {
+         getEntityService($scope, $http, $q);
+      };
 
-	$scope.getCurrentIdea = function() {
-	    getIdeaService($scope, $http, $q); 
-	};
-	
-	$scope.getThisIdea = function(ideaID) {
-	    $scope.currentIdeaID = ideaID; 
-	    console.log(ideaID); 
-	    $scope.getCurrentIdea(); 
-	};
+      $scope.getThisEntity = function (sep_dir) {
+         $scope.sep_dir = sep_dir;
+         $scope.getCurrentEntity();
+      };
 
-	$scope.getCurrentThinker = function () {
-            getThinkerService($scope, $http, $q);
-	};
+      $scope.showThinkerCol = function (col) { //determine if a thinker column should show or not.                  
+         return col <= $scope.thinkerColumns.length;
+      };
 
-	$scope.getThisThinker = function (thinkerID) {
-            $scope.currentThinkerID = thinkerID;
-            $scope.getCurrentThinker();
-	};
+      $scope.showIdeaCol = function (col) { //determine if a thinker column should show or not.                  
+         return col <= $scope.ideaColumns.length;
+      };
 
-	$scope.getNewThinker = function () {
-            $scope.getThisThinker($scope.newThinkerID);
-	};
+      setTimeout($scope.getCurrentEntity(), 50);
+   });
 
-	$scope.entities = [];
-	$scope.labels = [];
-	setTimeout($scope.getCurrentThinker(), 50);
-	$timeout(function () { getEntities($scope, $http, $q) }, 50);
-    });
+   function getEntities(scope, http, $q) {
 
-    function getEntities(scope, http, $q) {
+      var defer = $q.defer();
 
-	var defer = $q.defer();
+      http({ method: 'GET', url: host + "//entity.json?sep_filter=True" }).
+               success(function (data, status, headers, config) {
+                  defer.resolve(data.responseData.results);
+               }).
+               error(function (data, status, headers, config) {
+                  defer.reject();
+               });
 
-	http({ method: 'GET', url: host + "//entity.json?sep_filter=True" }).
-            success(function (data, status, headers, config) {
-		defer.resolve(data.responseData.results);
-            }).
-            error(function (data, status, headers, config) {
-		defer.reject();
-            });
+      defer.promise.then(function (results) {
+         scope.entities = results;
+         for (var i = 0; i < scope.entities.length; i++) {
+            var entity = scope.entities[i];
+            scope.labels.push(entity.label);
+         }
+      });
+   }
 
-	defer.promise.then(function (results) {
-            scope.entities = results;
-            for (var i = 0; i < scope.entities.length; i++) {
-		var entity = scope.entities[i];
-		scope.labels.push(entity.label);		
+   crossRef.directive('autoComplete', function ($timeout) {
+      return function (scope, iElement, iAttrs) {
+         iElement.autocomplete({
+            source: scope[iAttrs.uiItems],
+            select: function () {
+               $timeout(function () {
+                  iElement.trigger('input');
+                  var value = iElement.val();
+                  for (var i = 0; i < scope.entities.length; i++) {
+                     var entity = scope.entities[i];
+                     if (entity.label === value) {
+                        scope.sep_dir = scope.entities[i].sep_dir;
+                        scope.getCurrentEntity();
+                        break;
+                     }
+                  }
+               }, 0);
             }
-	});
-    }
+         });
+      };
+   });
 
-    
-    function getIdeaService(scope, http, $q) { //retrieve one thinker at a time using http service provided by AngularJS.  Recommend to stick with AngularJS
+   function getEntityService(scope, http, $q) {
 
-	http({ method: 'GET', url: host + "/idea/" + scope.currentIdeaID + ".json" }).
-	    success(function (data, status, headers, config) {
-		console.log(data); 
-		scope.currentIdea = data;
-		scope.currentIdea.thinkerCrossRef = [];
-		//getCrossRefs(scope, http, $q);
-	    }).
-	    error(function (data, status, headers, config) {
-		alert("Error:" + status);
-	    });
-    }
-    
-    function getThinkerService(scope, http, $q) { //retrieve one thinker at a time using http service provided by AngularJS.  Recommend to stick with AngularJS
+      http({ method: 'GET', url: host + "/entity.json?sep=" + scope.sep_dir }).
+               success(function (data, status, headers, config) {
+                  scope.entity = data.responseData.results[0];
+                  getCrossRefs(scope, http, $q);
+               }).
+               error(function (data, status, headers, config) {
+                  alert("Error:" + status);
+               });
+   }
 
-	http({ method: 'GET', url: host + "/thinker/" + scope.currentThinkerID + ".json" }).
-            success(function (data, status, headers, config) {
-		scope.currentThinker = data;
-		scope.currentThinker.thinkerCrossRef = [];
-		getCrossRefs(scope, http, $q);
-            }).
-            error(function (data, status, headers, config) {
-		alert("Error:" + status);
-            });
-    }
-    
-    function getIdeaCrossRefs(scope, http, $q){
-	
-	var occurrences = getIdeaRefAsyn(scope.currentIdeaID, http, "/idea/$id/thinker_occurrences.json?sep_filter=True", $q);
-	occurrences.then(function (results) { 
-	    
-	    var relatedterms = getideaRefAsyn(scope.currentIdeaID, http, "/idea/$id/related_thinkers.json?sep_filter=True", $q);
-	    relatedterms.then(function (results){
-            });
-	});
-    }
+   function BuildUriForReference(type, id, column) {
+      //   /thinker/$id/occurrences.json?sep_filter=True
+      //   /idea/$id/hyponyms.json?sep_filter=True
+      return "/" + type + "/" + id + "/" + column + ".json?sep_filter=True";
+   }
 
-    function getCrossRefs(scope, http, $q) {
+   //Each call pushs the data into results so that all can be processed later. We will also use the index to indicate which column the 
+   // data is for because we don't know the order of the returns.
+   function getCrossRefAsyn(http, uri, $q, results, index) {
 
-	var occurrences = getCrossRefAsyn(scope.currentThinkerID, http, "/occurrences.json?sep_filter=True", $q);
-	occurrences.then(function (results) {
+      var defer = $q.defer();
 
-	    populateCrossRef(scope.currentThinker.thinkerCrossRef, "occurrencesRanking", results);
+      http({ method: 'GET', url: host + uri }).
+          success(function (data, status, headers, config) {
+             results.push({ "index": index, "data": data.responseData.results });
+             defer.resolve();
+          }).
+          error(function (data, status, headers, config) {
+             defer.reject();
+          });
 
-	    var related = getCrossRefAsyn(scope.currentThinkerID, http, "/related.json?sep_filter=True", $q);
-	    related.then(function (results) {
+      return defer.promise;
+   }
 
-		populateCrossRef(scope.currentThinker.thinkerCrossRef, "relatedRanking", results);
+   function getCrossRefs(scope, http, $q) {
 
-		var hyponyms = getCrossRefAsyn(scope.currentThinkerID, http, "/hyponyms.json?sep_filter=True", $q);
-		hyponyms.then(function (results) {
+      var type = scope.entity.type;
+      var id = scope.entity.ID;
+      scope.thinkers = [];
+      scope.ideas = [];
 
-		    populateCrossRef(scope.currentThinker.thinkerCrossRef, "hyponymsRanking", results);
-		    calculateOverallRanking(scope.currentThinker.thinkerCrossRef);
-		    sortOverallRanking(scope.currentThinker.thinkerCrossRef);
-		    //var influenced = getCrossRefAsyn(scope, http, "/influenced.json?sep_filter=True", $q); //this api returns server error.
-		    /*
-		      influenced.then(function (results) {
-		      alert("didn't get to here");
-		      populateCrossRef(scope, "influencedRanking", results);
-		      });
-		    */
-		});
-	    });
-	});
-    }
+      if (type === "thinker") { //the column headings are used to contruct ulr based on the type and id.
+         //For thinker-idea
+         //   /thinker/$id/idea_occurrences.json?sep_filter=True
+         //   /thinker/$id/related_ideas.json?sep_filter=True
+         scope.ideaColumns = ["idea_occurrences", "related_ideas"];
 
-    var rankingTypes = ["occurrencesRanking", "relatedRanking", "hyponymsRanking", "influencedRanking"];
+         //For thinker
+         //   /thinker/$id/occurrences.json?sep_filter=True
+         //   /thinker/$id/related.json?sep_filter=True
+         //   /thinker/$id/hyponyms.json?sep_filter=True
+         //   /thinker/$id/influenced.json?sep_filter=True
+         scope.thinkerColumns= ["occurrences", "related", "hyponyms"];//, "influenced"];
+      }
+      else if (type === "idea") {
+         //For idea
+         //   /idea/$id/occurrences.json?sep_filter=True
+         //   /idea/$id/related.json?sep_filter=True
+         //   /idea/$id/hyponyms.json?sep_filter=True
+         //   /idea/$id/evaluated.json?sep_filter=True
+         //   /idea/$id/first_order.json?sep_filter=True
+         scope.ideaColumns = ["occurrences", "related", "hyponyms", "evaluated", "first_order"];
 
-    function calculateOverallRanking(thinkers) {
+         //For idea-thinker
+         //   /idea/$id/thinker_occurrences.json?sep_filter=True
+         //   /idea/$id/related_thinkers.json?sep_filter=True
+         scope.thinkerColumns = ["thinker_occurrences", "related_thinkers"];
+      }
+      else {
+         alert("entity type " + type + " is not supported");
+         return;
+      }
 
-	for (var i = 0; i < thinkers.length; i++) {//add all rankings together for a thinker
-	    var thinker = thinkers[i];
-	    var overallRanking = 0;
-	    var numOfSources = 0;
-	    for (var j = 0; j < rankingTypes.length; j++) {
-		var ranking = thinker[rankingTypes[j]];
-		if (!isNaN(ranking)) {
-		    overallRanking += ranking;
-		    numOfSources++;
-		}
-	    }
+      populateOneReferenceType(http, $q, scope.thinkers, scope.thinkerColumns, type, id);
+      populateOneReferenceType(http, $q, scope.ideas, scope.ideaColumns, type, id);
+   }
 
-	    thinker.overallRanking = overallRanking;
-	    thinker.numOfSources = numOfSources;
-	}
-    }
+   function populateOneReferenceType(http, $q, references, columns, type, id) {
 
+      var results = [];
+      var promises = [];
 
+      for (var i = 0 ; i < columns.length; i++) { //can I reply on the single thread of javascript to do a loop here?
+         var column = columns[i];
+         promises.push(getCrossRefAsyn(http, BuildUriForReference(type, id, column), $q, results, i));
+      }
 
-    function sortOverallRanking(thinkers) {
-	thinkers.sort(function (a, b) {
+      $q.all(promises).then(function () {
+         populateCrossRef(references, results, columns.length);
+      });
+   }
 
-	    if (a.numOfSources != b.numOfSources)
-		return b.numOfSources - a.numOfSources; //descending for number of sources
-	    else 
-		return a.overallRanking - b.overallRanking; //ascending for the ranking.
-	});
-    }
+   //crossRefs: could be thinkers or ideas, results: the array from cross ref calls.
+   function populateCrossRef(references, results, numOfColumns) {
 
-    function getCrossRefAsyn(thinkerID, http, uri, $q) {
+      for (var i = 0; i < results.length; i++) {
+         populateCrossRefForOneColumn(references, results[i].index, results[i].data);
+      }
 
-	var defer = $q.defer();
+      calculateOverallRanking(references, numOfColumns);
 
-	http({ method: 'GET', url: host + "/thinker/" + thinkerID + uri }).
-	    success(function (data, status, headers, config) {
-		defer.resolve(data.responseData.results);
-	    }).
-	    error(function (data, status, headers, config) {
-		defer.reject();
-	    });
+      sortOverallRanking(references);
+   }
 
-	return defer.promise;
-    }
+   //Because the column names for idea and thinker could be different, depending the type, here we use col0, col1 etc
+   //to represent the ranking column name.  This allows us to generically process the different column and types.
+   function populateCrossRefForOneColumn(referencs, colIndex, data) {
 
-    function getIdeaRefAsyn(ideaID, http, uri, $q) {
+      var colName = "col" + colIndex;
+      
+      for (var i = 0; i < data.length; i++) {
 
-	var defer = $q.defer();
+         var ref = data[i];
 
-	http({ method: 'GET', url: host + "/idea/" + ideaID + uri }).
-	    success(function (data, status, headers, config) {
-		defer.resolve(data.responseData.results);
-	    }).
-	    error(function (data, status, headers, config) {
-		defer.reject();
-	    });
+         var existingReferece = findReferenceByLabel(ref.label, referencs);
 
-	return defer.promise;
-    }
+         if (existingReferece != null) {
+            existingReferece[colName] = i + 1;
+         }
+         else {
+            ref[colName] = i + 1;
+            referencs.push(ref);
+         }
+      }
+   }
 
+   //referencs could be ideas or thinkers
+   function findReferenceByLabel(label, references) {
 
-    function populateCrossRef(thinkers, rankingType, results) {
+      var ret = null;
 
-	for (var i = 0; i < results.length; i++) {
-	    var thinker = results[i];
+      for (var i = 0; i < references.length; i++) {
+         if (label === references[i].label) {//the label of the refrence already exists in the array.
+            ret = references[i];
+            break;
+         }
+      }
 
-	    var existingThinker = findThinkerByLabel(thinker.label, thinkers);
+      return ret;
+   }
 
-	    if (existingThinker != null) {
-		existingThinker[rankingType] = i + 1;
-	    }
-	    else {
-		thinker[rankingType] = i + 1;
-		thinkers.push(thinker);
-	    }
-	}
-    }
+   function calculateOverallRanking(references, numOfColumns) {
 
-    function findThinkerByLabel(thinkerLabel, thinkers) {
+      for (var i = 0; i < references.length; i++) {//add all rankings together for a thinker
 
-	var ret = null;
-	3
-	for (var i = 0; i < thinkers.length; i++) {
-	    if (thinkerLabel === thinkers[i].label) {//the thinker already exists in the array.
-		ret = thinkers[i];
-		break;
-	    }
-	}
+         var ref = references[i];
+         var overallRanking = 0;
+         var numOfSources = 0;
 
-	return ret;
-    }
+         for (var j = 0; j < numOfColumns; j++) {
+            var ranking = ref["col" + j];
+            if (!isNaN(ranking)) {
+               overallRanking += ranking;
+               numOfSources++;
+            }
+         }
 
-    crossRef.directive('autoComplete', function ($timeout) {
-	return function (scope, iElement, iAttrs) {
-	    iElement.autocomplete({
-		source: scope[iAttrs.uiItems],
-		select: function () {
-		    $timeout(function () {
-			iElement.trigger('input');
-			var value = iElement.val();
-			for (var i = 0; i < scope.entities.length; i++) {
-			    var entity = scope.entities[i];
-			    if (entity.label === value) {
-				if (entity.type === "thinker"){
-			        	scope.currentThinkerID = entity.ID;
-				   scope.getCurrentThinker();
-                                }
-				else {//
-//do stuff for ideas
-alert("idea");
-}
-				  break;
-			    }
-			}
-		    }, 0);
-		}
-	    });
-	};
-    });
+         ref.overallRanking = overallRanking;
+         ref.numOfSources = numOfSources;
+      }
+   }
+
+   function sortOverallRanking(references) {
+      references.sort(function (a, b) {
+
+         if (a.numOfSources != b.numOfSources)
+            return b.numOfSources - a.numOfSources; //descending for number of sources
+         else
+            return a.overallRanking - b.overallRanking; //ascending for the ranking.
+      });
+   }
+
 })(window, jQuery);
